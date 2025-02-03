311a412cafc2742ef87718217174506c6421f4fc
Jose Antonio Cerón <jaceron95@gmail.com>
2025-02-03 18:13:42 +0100

    CPP01 finished

--- CPP01/ex06/Harl.cpp
+++ CPP01/ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: jceron-g <jceron-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 11:53:48 by jceron-g          #+#    #+#             */
-/*   Updated: 2025/01/28 13:05:59 by jceron-g         ###   ########.fr       */
+/*   Updated: 2025/02/03 18:10:57 by jceron-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,36 +22,68 @@ Harl :: ~Harl()
 	return ;
 }
 
-void Harl :: debug(void)
+static int levelToInt(std::string level)
 {
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	for (int i = 0; i < 4; i++)
+	{
+		if (level == levels[i])
+			return (i);
+	}
+	return (-1);
+}
+void Harl :: debug(void)
+{	
+	std::cout << "[ DEBUG ]" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!";
 }
 void Harl :: info(void)
 {
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+	std::cout << "[ INFO ]" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!";
 }
 void Harl :: warning(void)
 {
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+	std::cout << "[ WARNING ]" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month.";
 }
 void Harl :: error(void)
 {
-	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << "[ ERROR ]" << std::endl;
+	std::cout << "This is unacceptable! I want to speak to the manager now.";
 }
 
 void Harl :: complain(std::string level)
-{
-	std::string message[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-	function functionPointer[4] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
-
-	for (int i = 0; i < 4; i++)
+{	
+	int message = levelToInt(level);
+	switch (message)
 	{
-		if (message[i] == level)
+		case 0:
 		{
-			(this->*functionPointer[i])();
-			return;
+			this->debug();
+			std::cout << std::endl;
+			break;
 		}
-	}
-	std::cout << "Please write a correct message: DEBUG | INFO | WARNING | ERROR" << std::endl;
+		case 1:
+		{
+			this->info();
+			std::cout << std::endl;
+			break;
+		}
+		case 2:
+		{
+			this->warning();
+			std::cout << std::endl;
+			break;
+		}
+		case 3:
+		{
+			this->error();
+			std::cout << std::endl;
+			break;
+		}
+		default:
+			std::cout << "Error: Write a correct level: DEBUG | INFO | WARNING | ERROR" << std::endl;
+	}	
 }
 
